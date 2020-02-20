f764c533bda67b83fea70de8d00331511f83548b
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-02-20 06:06:34 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -106,7 +106,17 @@ end
   end 
 end 
   
-  
+  def play
+  while over? == false
+    turn
+  end
+  if won?
+    puts "Congratulations #{winner}!"
+  elsif draw?
+    puts "Cat's Game!"
+  end
+end
+
 end 
 
 
